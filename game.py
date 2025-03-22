90b431ea81ef81514014da7e332997780f204ce2
zhyldyz1 <zh.toktorova@gmail.com>
2025-03-22 11:01:34 -0400

    adding new file

---
+++ game.py
@@ -0,0 +1,12 @@
+def tripler(s):
+    triples = 0
+    s = [ss.strip() for ss in s.split()][::-1]
+
+    for i in range(len(s) - 1):
+        if s[i - triples + 1] == 'Triple':
+            s[i - triples] *= 3
+
+            del s[i - triples + 1]
+            triples += 1
+
+    return ' '.join(s[::-1])
